c88d4b70664612a55b8db954ce0562c8657b87e2
alvaro <alvaroamav96@gmail.com>
2024-06-17 16:41:30 -0300

    estilo comprobnate

--- resources/views/comprobantes/pdf.blade.php
+++ resources/views/comprobantes/pdf.blade.php
@@ -1,21 +1,18 @@
 @php
 $heads = [
-    'Item',
-    'Tipo',
-    'Valor Unitario',
-    'Cantidad',
-    'Subtotal',
-    'Impuestos',
-    'Total',
-    'Mascota',
+    'cantidad',
+    'descripcion',
+    'tipo',
+    'vu',
+    'subtotal',
+    // 'Impuestos',
+    'importe',
 ];
 $heads_pagos = [
-    'Fecha',
-    'Medio de pago',
-    'Monto',
-    'Motivo',
-    'Movimiento',
-    'Opciones'
+    'fecha',
+    'tipo de pago',
+    'monto',
+   
 ];
 $config = [
     'language' => [
@@ -35,93 +32,156 @@ $config = [
 
     <body>
         <style>
-            nav,
-            aside,
-            .dataTables_wrapper > div:first-child,
-            .dataTables_wrapper > div:last-child {
-                display: none !important;
-            }
-
-            .content-wrapper {
-                margin: 0 !important;
+            body {
                 font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                padding: 20px;
+                background-color: #f8f9fa;
             }
-
-                .content-wrapper input {
-                    box-sizing: border-box;
-                }
-
+        
+            .card {
+                border: 1px solid #dee2e6;
+                border-radius: .25rem;
+                box-shadow: 0 0.5rem 1rem rgba(0,0,0,.05);
+            }
+        
+            .card-header {
+                background-color: #28a745;
+                color: white;
+                font-weight: bold;
+            }
+        
+            .card-body {
+                padding: 1.25rem;
+            }
+        
+            .card-body h5 {
+                margin-bottom: 1rem;
+                color: #28a745;
+            }
+        
+            .card-body label {
+                display: block;
+                margin-bottom: .5rem;
+            }
+        
+            .card-body .row {
+                margin-bottom: 2rem;
+            }
+        
+            .card-body .row:last-child {
+                margin-bottom: 0;
+            }
+        
+            table {
+                width: 100%;
+                margin-bottom: 1rem;
+                color: #212529;
+                border-collapse: collapse;
+            }
+        
+            table th {
+                padding: .75rem;
+                vertical-align: top;
+                border-top: 1px solid #dee2e6;
+                background-color: #e9ecef;
+                color: #495057;
+            }
+        
+            table td {
+                padding: .75rem;
+                vertical-align: top;
+                border-top: 1px solid #dee2e6;
+            }
+        
             ul {
-                list-style-type: none;
-                text-align: right;
+                padding-left: 0;
+                list-style: none;
+            }
+        
+            ul li {
+                display: flex;
+                justify-content: space-between;
+                margin-bottom: .25rem;
+            }
+        
+            ul li:last-child {
+                font-weight: bold;
+                color: #28a745;
             }
 
-                ul li {
-                    display: grid;
-                    grid-template-columns: 1fr auto;
-                    gap: 25px;
-                }
-
-            #tabla-pagos tr th:last-child,
-            #tabla-pagos tr td:last-child {
-                display: none;
+            .small-font {
+                font-size: 0.8em;
             }
+
+            .header {
+                width: 100%;
+                margin-bottom: 5px;
+            }
+
+            .company-info, {
+                width: 100%; /* Reduced width to account for padding and border */
+                padding: 5px;
+                border: 1px solid #dee2e6;
+                border-radius: .25rem;
+                box-shadow: 0 0.5rem 1rem rgba(0,0,0,.05);
+            }
+
+  
         </style>
 
-        <div class="card card-success">
-            <div class="card-header">
-                <h3 class="card-title">Comprobante N° {{ str_pad($comprobante->serie, 3, '0', STR_PAD_LEFT) }}-{{ str_pad($comprobante->comprobante, 8, '0', STR_PAD_LEFT) }}</h3>
-            </div>
+        <div class="header">
+            <div class="company-info small-font">
 
+                <p>Dirección: Lima 4500</p>
+                <p>Email: veterinario@gmail.com</p>
+                <p>Número: 113151651</p>
+                <p>RUC 2343243423</p>
+                <p>Comprobante {{ str_pad($comprobante->serie, 3, '0', STR_PAD_LEFT) }}-{{ str_pad($comprobante->comprobante, 8, '0', STR_PAD_LEFT) }}</p>
+            </div>
+           
+        </div>
+
+        <div class="card">
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-12">
-                        <h5 class="d-block">Cliente</h5>
-                    </div>
-
-                    <div class="mt-4 col-md-12">
-                        <label class="w-100">Nombre y apellido
-                            <input type="text" class="form-control form-control-sm" name="cliente" value="{{ $comprobante->cliente->Nombre }} {{ $comprobante->cliente->Apellido }}" placeholder="" readonly>
+                        <label class="w-100">
+                            <strong>Adquiriente</strong>
+                           {{ $comprobante->cliente->Nombre }} {{ $comprobante->cliente->Apellido }}
                         </label>
-                        <label class="w-100">Cliente
-                            <input type="number" class="form-control form-control-sm" name="documento_identidad" value="{{ $comprobante->cliente->DocumentoIdentidad }}" placeholder="" min="0" readonly>
+                        <label class="w-100">
+                            <strong>Documento</strong>
+                            {{ $comprobante->cliente->DocumentoIdentidad }}
                         </label>
-                        <label class="w-100">Cliente
-                            <input type="date" class="form-control form-control-sm" name="fecha_nacimiento" value="{{ $comprobante->cliente->FechaNacimiento }}" placeholder="" min="0" readonly>
+                        <label class="w-100">
+                            <strong>Email</strong>
+                            {{ $comprobante->cliente->Email }}
                         </label>
-                        <label class="w-100">Email
-                            <input type="text" class="form-control form-control-sm" name="email" value="{{ $comprobante->cliente->Email }}" placeholder="" readonly>
-                        </label>
-                        <label class="w-100">Departamento
-                            <input type="text" class="form-control form-control-sm" name="departamento" value="{{ $comprobante->cliente->departamento->Departamento }}" placeholder="" readonly>
-                        </label>
-                        <label class="w-100">Provincia
-                            <input type="text" class="form-control form-control-sm" name="provincia" value="{{ $comprobante->cliente->provincia->Provincia }}" placeholder="" readonly>
-                        </label>
-                        <label class="w-100">Distrito
-                            <input type="text" class="form-control form-control-sm" name="distrito" value="{{ $comprobante->cliente->distrito->Distrito }}" placeholder="" readonly>
-                        </label>
-                        <label class="w-100">Dirección
-                            <input type="text" class="form-control form-control-sm" name="direccion" value="{{ $comprobante->cliente->Direccion ?? '-' }}" placeholder="" readonly>
+                        
+                        <label class="w-100">
+                            <strong>Dirección</strong> {{ $comprobante->cliente->Direccion ?? '-' }}
                         </label>
                     </div>
                 </div>
 
-                <div class="mt-4 row">
+            </div>
+        </div>
+
+                <div class="row">
                     <div class="col-md-12">
                         <h5 class="d-block">Venta</h5>
 
                         <x-adminlte-datatable id="tabla-items" :heads="$heads" head-theme="light" striped hoverable bordered compressed beautify :config="$config">
                             @foreach ($comprobante->venta->items as $item)
                                 <tr>
+                                    <td><span class="unidades-item">{{ intval($item->cantidad) }}</span></td>
                                     <td>{!! !is_null($item->servicio_id) ? ($item->servicio->Servicio ?? '<span class="text-danger">Servicio no encontrado</span>') : ($item->producto->Producto ?? '<span class="text-danger">Producto no encontrado</span>') !!}</td>
                                     <td>{{ !is_null($item->servicio_id) ? 'Servicio' : 'Producto' }}</td>
                                     <td>{{ Prices::symbol() }} <span class="precio-item precio-unitario">{{ number_format(($item->subtotal / $item->cantidad), 2, ',', '') }}</span></td>
-                                    <td>x <span class="unidades-item">{{ intval($item->cantidad) }}</span> u.</td>
+                                   
                                     <td>{{ Prices::symbol() }} <span class="precio-item precio-subtotal">{{ number_format($item->subtotal, 2, ',', '') }}</span></td>
-                                    <td>{{ Prices::symbol() }} <span class="precio-item precio-impuestos">{{ number_format($item->impuestos, 2, ',', '') }}</span></td>
+                                    {{-- <td>{{ Prices::symbol() }} <span class="precio-item precio-impuestos">{{ number_format($item->impuestos, 2, ',', '') }}</span></td> --}}
                                     <td>{{ Prices::symbol() }} <span class="precio-item precio-total">{{ number_format($item->total, 2, ',', '') }}</span></td>
-                                    <td>{!! $item->mascota->Mascota ?? '<span class="text-danger">Mascota no encontrada</span>' !!}</td>
                                 </tr>
                             @endforeach
                         </x-adminlte-datatable>
@@ -138,7 +198,7 @@ $config = [
                     </div>
                 </div>
 
-                <div class="mt-4 row">
+                <div class="row">
                     <div class="col-md-12">
                         <h5 class="d-block">Pagos</h5>
 
@@ -150,21 +210,7 @@ $config = [
                                             <td>{{ $pago->created_at }}</td>
                                             <td>{{ $pago->medio_pago->FormaDePago }}</td>
                                             <td class="{{ $pago->tipo_movimiento_id == 3 ? 'text-danger' : 'text-success' }}">{{ Prices::symbol() }} {{ number_format($pago->importe, 2, ',', '') }}</td>
-                                            <td>{{ $pago->motivo ?? '—' }}</td>
-                                            <td>{{ $pago->tipo_movimiento->nombre }}</td>
-                                            <td>
-                                                @if (!$pago->anulado)
 
-                                                    <a href="" class="eliminar-item" data-pago_id="{{ $pago->id }}" title="Anular este pago">
-                                                        <i class="fas fa-fw fa-trash"></i>
-                                                    </a>
-
-                                                @else
-
-                                                    —
-
-                                                @endif
-                                            </td>
                                         </tr>
                                     @endforeach
                                 </x-adminlte-datatable>
@@ -180,7 +226,6 @@ $config = [
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+            
     </body>
 </html>
